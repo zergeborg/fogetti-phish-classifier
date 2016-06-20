efca4db1c8d6ef5b58785e112c13e431803317f9
Fogetti <fogetti@gmail.com>
2016-06-20 16:11:06 -0700

    KafkaIntersectionBolt returns Base64 encoded URLs

--- src/main/java/fogetti/phish/storm/relatedness/intersection/KafkaIntersectionBolt.java
+++ src/main/java/fogetti/phish/storm/relatedness/intersection/KafkaIntersectionBolt.java
@@ -1,6 +1,13 @@
 package fogetti.phish.storm.relatedness.intersection;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+import java.util.Map;
+
 import org.apache.storm.redis.common.config.JedisPoolConfig;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
 import org.apache.storm.tuple.Tuple;
 
 import fogetti.phish.storm.relatedness.KafkaSpout.KafkaMessageId;
@@ -8,15 +15,24 @@ import fogetti.phish.storm.relatedness.KafkaSpout.KafkaMessageId;
 public class KafkaIntersectionBolt extends IntersectionBolt {
 
     private static final long serialVersionUID = 6092407511669593227L;
+    private Encoder encoder;
 
     public KafkaIntersectionBolt(JedisPoolConfig config) {
         super(config);
     }
     
     @Override
+    @SuppressWarnings("rawtypes")
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+        super.prepare(stormConf, context, collector);
+        this.encoder = Base64.getEncoder();
+    }
+    
+    @Override
     protected String getInputURL(Tuple input) {
         KafkaMessageId data = (KafkaMessageId)input.getValueByField("url");
-        String url = data.value;
+        String encodedURL = encoder.encodeToString(data.value.getBytes(StandardCharsets.UTF_8));
+        String url = encodedURL;
         return url;
     }
 
