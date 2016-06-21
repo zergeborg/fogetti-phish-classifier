3ec96a63df292be1b268bfff050a06ef6f4ee3aa
Fogetti <fogetti@gmail.com>
2016-06-20 17:48:36 -0700

    Fixed the verdict

--- src/main/java/fogetti/phish/storm/relatedness/intersection/ClassifierBolt.java
+++ src/main/java/fogetti/phish/storm/relatedness/intersection/ClassifierBolt.java
@@ -297,9 +297,9 @@ public class ClassifierBolt extends AbstractRedisBolt {
     private String makeVerdict(double[] dist) {
         String verdict = "SAFE";
         double invalid = dist[1];
+        if (invalid < 0.9 && invalid > 0.1) verdict = "CAN'T DETERMINE";
         if (invalid <= 0.1) verdict = "SAFE";
         if (invalid >= 0.9) verdict = "UNSAFE";
-        else verdict = "CAN'T DETERMINE";
         return verdict;
     }
 
